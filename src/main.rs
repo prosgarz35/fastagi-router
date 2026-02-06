ae51d9cea78c4c21db3750f6143e6dae5fa8a6d9
ilya terskov <prosgarz35@gmail.com>
2026-02-07 01:39:08 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,42 @@
 use std::io::{self, BufRead, Write};
 
 const MAPPINGS: &[(&str, &str)] = &[
-    ("79235253998", "501"), ("73843602313", "501"),
-    ("79235254061", "502"), ("73843601773", "502"),
-    ("79235254150", "503"),
-    ("79235254132", "504"), ("73843602414", "504"),
-    ("79235254389", "505"),
-    ("79235254439", "506"), ("73843601771", "506"),
-    ("79235254667", "507"), ("73843600912", "507"),
-    ("79235254706", "508"), ("73843600911", "508"),
-    ("79235255049", "509"), ("73843601331", "509"),
-    ("79235255136", "510"), ("73843601221", "510"),
-    ("79234688941", "511"),
-    ("79234689436", "512"),
-    ("79234693619", "513"),
-    ("79234693746", "514"),
-    ("79234693868", "515"),
-    ("79234698651", "516"),
-    ("79234698906", "517"),
-    ("79234702567", "518"),
-    ("79235069558", "519"),
-    ("79235237068", "520"),
+    ("79235253998","501"),("73843602313","501"),
+    ("79235254061","502"),("73843601773","502"),
+    ("79235254150","503"),
+    ("79235254132","504"),("73843602414","504"),
+    ("79235254389","505"),
+    ("79235254439","506"),("73843601771","506"),
+    ("79235254667","507"),("73843600912","507"),
+    ("79235254706","508"),("73843600911","508"),
+    ("79235255049","509"),("73843601331","509"),
+    ("79235255136","510"),("73843601221","510"),
+    ("79234688941","511"),("79234689436","512"),
+    ("79234693619","513"),("79234693746","514"),
+    ("79234693868","515"),("79234698651","516"),
+    ("79234698906","517"),("79234702567","518"),
+    ("79235069558","519"),("79235237068","520"),
 ];
 
-fn normalize(s: &str) -> Option<String> {
-    let d: String = s.chars().filter(|c| c.is_ascii_digit()).collect();
-    Some(match d.len() {
-        6  => format!("73843{d}"),
-        10 => format!("7{d}"),
-        11 if d.starts_with('8') => format!("7{}", &d[1..]),
-        11 if d.starts_with('7') => d,
-        _  => return None,
-    })
-}
+fn main() {
+    for l in io::stdin().lock().lines() { if l.map_or(true, |s| s.trim().is_empty()) { break; } }
 
-fn main() -> io::Result<()> {
-    for line in io::stdin().lock().lines() {
-        if line.ok().map_or(true, |s| s.trim().is_empty()) { break; }
-    }
+    let mut args = std::env::args().skip(1);
+    let mode = args.next().unwrap_or_default();
+    let dialed: String = args.next().unwrap_or_default().chars().filter(|c| c.is_ascii_digit()).collect();
+    let dialed = match dialed.len() {6 => format!("73843{dialed}"),10 => format!("7{dialed}"),11 if dialed.starts_with('8') => format!("7{}", &dialed[1..]), _ => dialed};
+    let caller = args.next().unwrap_or_default();
 
-    let args: Vec<_> = std::env::args().collect();
-    let [_, mode, dialed, caller @ ..] = args.as_slice() else {
-        println!(r#"SET VARIABLE LOOKUP_SUCCESS "FALSE""#);
-        return Ok(());
+    let found = if mode=="in" {
+        if let Some(&(_,ext)) = MAPPINGS.iter().find(|&&(n,_)| n==dialed) { println!(r#"SET VARIABLE DIAL_TARGET "{ext}""#); true } else { false }
+    } else {
+        if let Some(&(trunk,_)) = MAPPINGS.iter().find(|&&(t,e)| t==caller || e==caller) { 
+            println!(r#"SET VARIABLE DIAL_TRUNK "{trunk}""#); 
+            println!(r#"SET VARIABLE DIAL_NUMBER "{dialed}""#); 
+            true 
+        } else { false }
     };
 
-    let caller = caller.first().map_or("", String::as_str);
-    let success = match mode.as_str() {
-        "in" => normalize(dialed)
-            .and_then(|n| MAPPINGS.iter().find(|&&(num,_)| num == n))
-            .map(|&(_, ext)| { println!(r#"SET VARIABLE DIAL_TARGET "{ext}""#); true }),
-
-        "out" => MAPPINGS.iter().find(|&&(n,e)| n == caller || e == caller)
-            .and_then(|&(trunk,_)| normalize(dialed).map(|num| {
-                println!(r#"SET VARIABLE DIAL_TRUNK "{trunk}""#);
-                println!(r#"SET VARIABLE DIAL_NUMBER "{num}""#);
-                true
-            })),
-
-        _ => None,
-    }.unwrap_or(false);
-
-    println!(r#"SET VARIABLE LOOKUP_SUCCESS "{}""#, if success { "TRUE" } else { "FALSE" });
-    io::stdout().flush()
+    println!(r#"SET VARIABLE LOOKUP_SUCCESS "{}""#, if found { "TRUE" } else { "FALSE" });
+    io::stdout().flush().ok();
 }
